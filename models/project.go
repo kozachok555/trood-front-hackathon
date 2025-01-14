e1a5e982e57ca1a58d434a7d9269e5382830aa35
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2025-01-14 21:10:21 +0100

    Split Frontend & Backend Into Different Directories

--- models/project.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Project struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Deadline    string `json:"deadline"`
-	Experience  string `json:"experience"`
-}
